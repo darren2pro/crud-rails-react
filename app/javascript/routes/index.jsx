c5a5facf976ffd61cc972c33407f0f9922f42897
Darren Heng <darren2pro@gmail.com>
2022-01-13 14:15:27 +0800

    The rails server continues to work with some challenges
    Problems:
    - Currently the application renders two lists of the beers which is not ideal

---
+++ app/javascript/routes/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../components/Home";
+
+export default (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+    </Routes>
+  </BrowserRouter>
+);
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :beers
+  namespace :api do
+    namespace :v1 do
+      get "beers/index"
+      post "beers/create"
+      delete "beers/:id", to: "beers#destroy"
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "beers#index"
 end

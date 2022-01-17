789ffaa1f1ec4f703eb8058a273da89b4b06b53f
Darren Heng <darren2pro@gmail.com>
2022-01-17 11:13:09 +0800

    Implement search functionality for filtering beers
    I create a new component called BeerList which will display the
    filtered beers that it receives.

--- app/javascript/components/SearchBar.jsx
+++ app/javascript/components/SearchBar.jsx
@@ -1,8 +1,18 @@
+import React from "react";
 import { Input } from 'antd';
 const { Search } = Input;
 
 const SearchBar = (props) => {
 
+    const onSearch = (event) => {
+        props.onSearch(event.target.value);
+    };
+
+    return (
+        <Search placeholder="input search text" enterButton="Search" size="middle" loading={true}
+                onSearch={onSearch} onChange={onSearch} onPressEnter={onSearch}
+                value={props.input ? props.input : ""} />
+    );
 };
 
 export default SearchBar;
--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../components/Home";
 import Beer from "../components/Beer"
+import SearchPage from "../components/SearchPage";
 
 export default (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home/>} />
         <Route path="/:id" element={<Beer/>} />
+        <Route path="/searchPage" exact element={<SearchPage/>} />
     </Routes>
   </BrowserRouter>
 );
